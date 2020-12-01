9b3fa0c46de3b54b9b14f7e98c642c452d9616c7
bedo_ <sci.bedomohamed2018@alexu.edu.eg>
2020-12-01 09:58:04 +0200

    Change app name

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
+        title: 'El-Gam3a',
         // home: CourseDetailsScreen(),
 
         initialRoute: SplashScreen.id,
