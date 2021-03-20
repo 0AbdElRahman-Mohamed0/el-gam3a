c2395cbd8a0492670eb8c0f47c1f020a518df4ba
kira <sci.bedoMohamed2018@alexu.edu.eg>
2021-03-20 13:39:06 -0400

    fix choose image

--- lib/widgets/pick_photo_pop_up.dart
+++ lib/widgets/pick_photo_pop_up.dart
@@ -15,51 +15,65 @@ class PickPhotoPopUp extends StatelessWidget {
     return CupertinoAlertDialog(
       title: Text('Choose how to set photo'),
       content: Column(
+        mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           SizedBox(height: 24.0),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              InkWell(
-                onTap: takePhotoOnPressed,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(4.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color(0xFF000000).withOpacity(0.34),
-                        offset: Offset(0, 3),
-                        blurRadius: 6,
-                      )
-                    ],
-                  ),
-                  child: Text(
-                    'Camera',
-                    style: TextStyle(
-                      color: Colors.white,
+              Expanded(
+                child: GestureDetector(
+                  onTap: takePhotoOnPressed,
+                  child: Container(
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(4.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0xFF000000).withOpacity(0.34),
+                          offset: Offset(0, 3),
+                          blurRadius: 6,
+                        )
+                      ],
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Camera',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ),
-              InkWell(
-                onTap: pickPhotoOnPressed,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).accentColor,
-                    borderRadius: BorderRadius.circular(4.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color(0xFF000000).withOpacity(0.34),
-                        offset: Offset(0, 3),
-                        blurRadius: 6,
-                      )
-                    ],
-                  ),
-                  child: Text(
-                    'Gallery',
-                    style: TextStyle(
-                      color: Colors.white,
+              SizedBox(
+                width: 8,
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: pickPhotoOnPressed,
+                  child: Container(
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).accentColor,
+                      borderRadius: BorderRadius.circular(4.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0xFF000000).withOpacity(0.34),
+                          offset: Offset(0, 3),
+                          blurRadius: 6,
+                        )
+                      ],
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Gallery',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ),
                 ),
