e1d341864396f609dc85df5c55c8237405fc7684
kira <sci.bedoMohamed2018@alexu.edu.eg>
2021-03-20 15:26:45 -0400

    app icon

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -4,7 +4,6 @@ import 'package:elgam3a/screens/home_screen.dart';
 import 'package:elgam3a/utilities/loading.dart';
 import 'package:elgam3a/widgets/input_text.dart';
 import 'package:elgam3a/widgets/leave_pop_up.dart';
-import 'package:elgam3a/widgets/login_app_bar.dart';
 import 'package:elgam3a/widgets/reset_password_pop_up.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flrx_validator/flrx_validator.dart';
@@ -205,131 +204,151 @@ class _LoginScreenState extends State<LoginScreen> {
     return WillPopScope(
       onWillPop: () => _youWantLeave(),
       child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(100.0),
-          child: LoginAppBar(
-            title: Text(
-              'El-Gam3a',
-              style: Theme.of(context).textTheme.button.copyWith(
-                    fontWeight: FontWeight.w700,
-                    fontSize: 26.0,
-                  ),
-            ),
-          ),
-        ),
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(vertical: 50.0, horizontal: 30.0),
-          child: Form(
-            key: _formKey,
-            autovalidateMode: _autoValidate
-                ? AutovalidateMode.always
-                : AutovalidateMode.disabled,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Center(
-                  child: SvgPicture.asset('assets/svg/big logo.svg'),
-                ),
-                SizedBox(height: 36),
-                InputText(
-                  validator: Validator(
-                    rules: [
-                      RequiredRule(validationMessage: 'ID is required.'),
-                      MinLengthRule(11,
-                          validationMessage: 'ID should be 11 number.'),
-                      MaxLengthRule(11,
-                          validationMessage: 'ID should be 11 number.'),
-                    ],
-                  ),
-                  inputType: 'email',
-                  maxLength: 11,
-                  onSaved: (value) {
-                    univCode = value;
-                  },
-                ),
-                SizedBox(
-                  height: 15.0,
-                ),
-                InputText(
-                  validator: Validator(
-                    rules: [
-                      RequiredRule(validationMessage: 'Password is required.'),
-                    ],
-                  ),
-                  inputType: 'password',
-                  onSaved: (value) {
-                    password = value;
-                  },
-                ),
-                SizedBox(
-                  height: 15.0,
-                ),
-                InkWell(
-                  child: Padding(
-                    padding: EdgeInsets.all(5.0),
-                    child: Text(
-                      'Forgot your password ?',
-                      style: Theme.of(context).textTheme.headline1.copyWith(
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.w500,
-                          ),
-                    ),
-                  ),
-                  onTap: () => _forgetPassword(),
-                ),
-                _emailSent
-                    ? Row(
-                        children: [
-                          Icon(
-                            Icons.check_circle_outline,
-                            color: Colors.green[300],
-                            size: 28,
-                          ),
-                          SizedBox(
-                            width: 4,
-                          ),
-                          Expanded(
-                            child: Text(
-                              'Reset email sent, please check your mail.',
-                              style: Theme.of(context).textTheme.bodyText1,
-                            ),
-                          ),
-                        ],
-                      )
-                    : SizedBox(),
-                SizedBox(
-                  height: 15.0,
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: InkWell(
-                    onTap: _login,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).cardColor,
-                        borderRadius: BorderRadius.circular(18.0),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color(0xFF000000).withOpacity(0.34),
-                            offset: Offset(0, 3),
-                            blurRadius: 6,
-                          )
-                        ],
-                      ),
-                      child: Center(
-                        child: Text(
-                          'Login',
-                          style: Theme.of(context).textTheme.button.copyWith(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w400,
-                              ),
+        body: LayoutBuilder(
+          builder: (context, constraints) => SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                  minWidth: constraints.maxWidth,
+                  minHeight: constraints.maxHeight),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 50.0, horizontal: 30.0),
+                child: Form(
+                  key: _formKey,
+                  autovalidateMode: _autoValidate
+                      ? AutovalidateMode.always
+                      : AutovalidateMode.disabled,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Center(
+                        child: SvgPicture.asset(
+                          'assets/svg/big logo.svg',
+                          height: 240,
+                          width: 240,
                         ),
                       ),
-                    ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: 20,
+                          ),
+                          InputText(
+                            validator: Validator(
+                              rules: [
+                                RequiredRule(
+                                    validationMessage: 'ID is required.'),
+                                MinLengthRule(11,
+                                    validationMessage:
+                                        'ID should be 11 number.'),
+                                MaxLengthRule(11,
+                                    validationMessage:
+                                        'ID should be 11 number.'),
+                              ],
+                            ),
+                            inputType: 'email',
+                            maxLength: 11,
+                            onSaved: (value) {
+                              univCode = value;
+                            },
+                          ),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          InputText(
+                            validator: Validator(
+                              rules: [
+                                RequiredRule(
+                                    validationMessage: 'Password is required.'),
+                              ],
+                            ),
+                            inputType: 'password',
+                            onSaved: (value) {
+                              password = value;
+                            },
+                          ),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          InkWell(
+                            child: Padding(
+                              padding: EdgeInsets.all(5.0),
+                              child: Text(
+                                'Forgot your password ?',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline1
+                                    .copyWith(
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                              ),
+                            ),
+                            onTap: () => _forgetPassword(),
+                          ),
+                          _emailSent
+                              ? Row(
+                                  children: [
+                                    Icon(
+                                      Icons.check_circle_outline,
+                                      color: Colors.green[300],
+                                      size: 28,
+                                    ),
+                                    SizedBox(
+                                      width: 4,
+                                    ),
+                                    Expanded(
+                                      child: Text(
+                                        'Reset email sent, please check your mail.',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText1,
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              : SizedBox(),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: InkWell(
+                          onTap: _login,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Theme.of(context).cardColor,
+                              borderRadius: BorderRadius.circular(18.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color(0xFF000000).withOpacity(0.34),
+                                  offset: Offset(0, 3),
+                                  blurRadius: 6,
+                                )
+                              ],
+                            ),
+                            child: Center(
+                              child: Text(
+                                'Login',
+                                style:
+                                    Theme.of(context).textTheme.button.copyWith(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
+              ),
             ),
           ),
         ),
